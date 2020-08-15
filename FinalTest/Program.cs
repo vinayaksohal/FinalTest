aa80c31b8da097347affc84829b725ba85e0945d
vinayak <vinayak.sohal007@gmail.com>
2020-08-15 17:43:19 -0400

    I checked in the evening code was not successfully pushed to Github, for your information code was last compiled before completion of test 10:35am, kindly check from your end, Thanks John

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -10,31 +10,56 @@ namespace FinalTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            FinalTest<int> finaltest = new FinalTest<int>();
+            finaltest.AddToCollection(11);
+            finaltest.AddToCollection(2);
+            finaltest.AddToCollection(3);
+            finaltest.AddToCollection(10);
+            finaltest.AddToCollection(14);
+
+            FinalTest<char> finaltest2 = new FinalTest<char>();
+            finaltest2.AddToCollection('v');
+            finaltest2.AddToCollection('a');
+            finaltest2.AddToCollection('b');
+            finaltest2.AddToCollection('v');
+            finaltest2.AddToCollection('c');
+
+
+            Console.WriteLine(finaltest.GetItemFromCollection(1));
+
+            Console.WriteLine();
+            Console.WriteLine("----Displaying List in Decending order---");
+            finaltest.SortDispaly();
+            finaltest2.SortDispaly();
+
+
+            FinalTest<float> finaltest1 = new FinalTest<float>();
+       
         }
     }
 
 
-    public class SortList<T> : IComparer<T> where T:struct
-    {
-        public int Compare(T x, T y)
-        {
-            if (x == 0 || y == )
-            {
-                return 0;
-            }
+    //public class SortList<T> : IComparer<T> where T:struct
+    //{
+    //    public int Compare(T x, T y)
+    //    {
+    //        if (x == 0 || y == )
+    //        {
+    //            return 0;
+    //        }
 
-            // CompareTo() method 
-            return x.CompareTo(y);
+    //        // CompareTo() method 
+    //        return x.CompareTo(y);
 
-        }
-    }
+    //    }
+    //}
 
 
     public class FinalTest<T> where T: struct
     {
         private List<T> PrivateGenericCollection;
 
-        FinalTest()
+        public FinalTest()
         {
             PrivateGenericCollection = new List<T>();
         }
@@ -49,15 +74,15 @@ namespace FinalTest
             return PrivateGenericCollection.ElementAt(index);
         }
 
-        public void Sort(List<T> l)
+        public void SortDispaly()
         {
-            List<T> SortedList = new List<T>(l);
 
-            SortList<T> sl = new SortList<T>;
+            //SortList<T> sl = new SortList<T>;
 
-            SortedList.Sort();
+            this.PrivateGenericCollection.Sort();
+            this.PrivateGenericCollection.Reverse();
 
-            foreach(T g in SortedList)
+            foreach(T g in PrivateGenericCollection)
             {
                 Console.WriteLine(g);
             }
